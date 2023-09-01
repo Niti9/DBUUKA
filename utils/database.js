c2b10078e430472747e994191e1e9df1c1939458
Niti9 <nnegi8298@gmail.com>
2023-09-01 16:04:44 +0530

    mongoose error test 3

--- utils/database.js
+++ utils/database.js
@@ -2,28 +2,27 @@ import mongoose from "mongoose";
 
 let isConnected = false; //track the connection
 
-const db = process.env.MONGODB_URI
-export const connectToDB = async ()=>{
-    mongoose.set('strictQuery',true);
+const db = process.env.MONGODB_URI;
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
 
-    if(isConnected){
-        console.log('MongoDB is already connected');
-        return;
-    }
-    //else condition
-    try{
-        await mongoose.connect(db,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName:"share_prompt",
-        })
-        isConnected = true;
-        console.log("MongoDB connected");
-    }
-    catch(error)
-    {
-        console.log(error);
-    }
-    
-}
-
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+  //else condition
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Nit:N.I.T.I.N@cluster0.0shci4t.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "share_prompt",
+      }
+    );
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
